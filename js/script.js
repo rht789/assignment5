79779864721d0974663f1e11fb6f460588224783
Md. Rezaul Karim <rezaulrahaat@gmail.com>
2025-08-29 23:22:27 +0600

    calling functionality alert and coin deducttion done

--- js/script.js
+++ js/script.js
@@ -10,4 +10,29 @@ document.addEventListener("DOMContentLoaded", function () {
       heartCountElement.innerText = heartCount;
     });
   }
+
+  const coinCountElement = document
+    .getElementById("coin-count")
+    .querySelector("p");
+  const callButtons = document.querySelectorAll(".call-button");
+  let coinCount = 100;
+
+  for (const callButton of callButtons) {
+    callButton.addEventListener("click", function () {
+      if (coinCount < 20) {
+        alert("You don't have enough coins! You need 20 coins to make a call.");
+        return;
+      }
+
+      const card = this.parentElement.parentElement;
+      const serviceName = card.querySelector(".service-name").innerText;
+      const hotlineNumber = card.querySelector(".hotline-number").innerText;
+
+      const alertMessage = `📞 Calling ${serviceName} ${hotlineNumber}...`;
+      alert(alertMessage);
+
+      coinCount -= 20;
+      coinCountElement.innerText = coinCount;
+    });
+  }
 });
